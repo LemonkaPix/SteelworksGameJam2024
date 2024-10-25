0d6bb1636145d0f03f351028bb03833c08c4ff3e
SideArker <rsowa126@gmail.com>
2024-10-25 23:45:41 +0200

    outline shader

--- Assets/_Game/Obstacles/Scripts/Trap.cs
+++ Assets/_Game/Obstacles/Scripts/Trap.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Trap : MonoBehaviour
 {
+    [SerializeField] bool triggered;
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.GetComponent<PlayerManager>()) return;
-        Debug.Log("Touched trap");
+        if (triggered) return;
+
+        if (other.gameObject.layer != LayerMask.NameToLayer("Player")) return;
+        Debug.Log("Player touch trap");
+        triggered = true;
     }
 }
