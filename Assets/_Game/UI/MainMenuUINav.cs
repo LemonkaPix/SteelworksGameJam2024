d6fa47663762b9862d3851e9ad70047d4777bc35
LemonkaPix <maciejwrobel029@gmail.com>
2024-10-27 02:22:55 +0100

    Update Room.unity

--- Assets/_Game/UI/MainMenuUINav.cs
+++ Assets/_Game/UI/MainMenuUINav.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuUINav : MonoBehaviour
 {
     [SerializeField] Vector3 resizeFrom;
     [SerializeField] Vector3 resizeTo;
+    [SerializeField] Camera cam;
+    [SerializeField] MeshRenderer tv;
+    [SerializeField] Material tvMat;
     bool resizingFinished = false;
     bool isResizing = false;
 
@@ -35,4 +39,16 @@ public class MainMenuUINav : MonoBehaviour
 
     }
 
+    public void StartGame()
+    {
+        cam.GetComponent<Animator>().Play("MainMenu");
+        tv.material = tvMat;
+        StartCoroutine(WaitForCam());
+    }
+
+    IEnumerator WaitForCam()
+    {
+        yield return new WaitUntil(() => cam.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !cam.GetComponent<Animator>().IsInTransition(0));
+        SceneManager.LoadScene(1);
+    }
 }
